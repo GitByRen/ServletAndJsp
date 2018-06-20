d5ce84449b013cfc56b80d3e6f7370bdb8131b9b
GitByRen <1402443141@qq.com>
2018-06-20 23:21:43 +0800

    servleContext

--- src/main/java/com/important/day1/HelloServlet.java
+++ src/main/java/com/important/day1/HelloServlet.java
@@ -1,10 +1,12 @@
 package com.important.day1;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Enumeration;
 
 import javax.servlet.Servlet;
 import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -49,14 +51,43 @@ public class HelloServlet implements Servlet {
 	@Override
 	public void init(ServletConfig servletConfig) throws ServletException {
 		System.out.println("init");
-		String initParameter = servletConfig.getInitParameter("user");
-		System.out.println("user:" + initParameter);
+		String user = servletConfig.getInitParameter("user");
+		System.out.println("user:" + user);
 		Enumeration<String> names = servletConfig.getInitParameterNames();
 		while (names.hasMoreElements()) {
 			String name = names.nextElement();
 			String value = servletConfig.getInitParameter(name);
 			System.out.println("name:" + name + ",value:" + value);
 		}
+
+		System.out.println("*******************");
+
+		ServletContext servletContext = servletConfig.getServletContext();
+		String driver = servletContext.getInitParameter("driver");
+		System.out.println("driver:" + driver);
+		Enumeration<String> names2 = servletContext.getInitParameterNames();
+		while (names2.hasMoreElements()) {
+			String name = names2.nextElement();
+			String value = servletContext.getInitParameter(name);
+			System.out.println("name:" + name + ",value:" + value);
+		}
+		
+		// 获取当前WEB应用的某一个文件在服务器上的绝对路径，而不是部署前的路径
+		String realPath = servletContext.getRealPath("/jdbc.properties");
+		System.out.println(realPath);
+		
+		// 获取当前WEB应用的名称
+		String contextPath = servletContext.getContextPath();
+		System.out.println(contextPath);
+		
+		// 获取当前WEB应用的某一个文件对应的输入流
+		ClassLoader classLoader = getClass().getClassLoader();
+		InputStream is1 = classLoader.getResourceAsStream("jdbc.properties");
+		System.out.println("1." + is1);
+
+		// /为当前WEB应用的根目录
+		InputStream is2 = servletContext.getResourceAsStream("/WEB-INF/classes/jdbc.properties");
+		System.out.println(is2);
 	}
 
 	@Override
